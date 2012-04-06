abdc2f961d0c88f98af2f98e86eb4f883dddc228
John J. Mikucki <john.j.mikucki@gmail.com>
2012-04-06 16:28:07 -0400

    finished codebreaker exercise and chapter 9

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,2 +1,3 @@
+$LOAD_PATH << File.join( File.dirname(File.expand_path(__FILE__)), "../lib/codebreaker")
 require 'codebreaker/game'
 require 'codebreaker/marker'
--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -9,10 +9,21 @@ module Codebreaker
         count + (exact_match?(index) ? 1:0)
       end
     end
-    def number_match_count
-      (0..3).inject(0) do |count,index|
-        count + (number_match?(index) ? 1:0)
+
+    def total_match_count
+      count = 0
+      secret = @secret.split('')
+      @guess.split('').map do |n|
+        if secret.include?(n)
+          secret.delete_at(secret.index(n))
+          count += 1
+        end
       end
+      count
+    end
+
+    def number_match_count
+      total_match_count - exact_match_count
     end
 
     def exact_match?(index)
